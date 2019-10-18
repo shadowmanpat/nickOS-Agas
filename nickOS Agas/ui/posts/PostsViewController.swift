fbcbc6814d3b830d97e6e94c2aaa143421ed2f15
nikolaos agas <agascompany>
2019-10-18 15:55:56 +0300

    initial commit

--- nickOS Agas/ui/posts/PostsViewController.swift
+++ nickOS Agas/ui/posts/PostsViewController.swift
@@ -8,11 +8,11 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class PostsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //Do any additional setup after loading the view.
     }
 
 
